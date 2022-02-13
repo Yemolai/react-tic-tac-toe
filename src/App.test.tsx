ebf56946b97d8f12007148a8ba1dd6e216c463e4
Leonardo Santos Severino <leonardo.osantos@mercadolivre.com>
2022-02-13 02:38:11 -0300

    refac: splits the simulated game into a function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -74,63 +74,49 @@ describe("App component", () => {
   });
 
   test('should Player A win when filling the first row', () => {
-    const winAlert = jest.spyOn(window, 'alert');
+    const winAlert = jest.spyOn(window, 'alert').mockImplementation();
     const playerAPlays = [/x0, y0/i, /x0, y1/i, /x0, y2/i]
     const playerBPlays = [/x1, y0/i, /x1, y1/i]
-    let boardMark;
 
-    playerAPlays.forEach((playerAPlay, index) => {
-      boardMark = screen.getByTestId(playerAPlay);
-      userEvent.click(boardMark);
-
-      if (playerBPlays[index] !== undefined) {
-        boardMark = screen.getByTestId(playerBPlays[index]);
-        userEvent.click(boardMark);
-      }
-    });
+    simulatePlayersPlaying(playerAPlays, playerBPlays);
 
     expect(winAlert).toHaveBeenCalledTimes(1);
     expect(winAlert).toHaveBeenCalledWith(expect.stringMatching(`Player A won! Congrats!`));
   });
 
   test('should Player A win when filling the second row', () => {
-    const winAlert = jest.spyOn(window, 'alert');
+    const winAlert = jest.spyOn(window, 'alert').mockImplementation();
     const playerAPlays = [/x1, y0/i, /x1, y1/i, /x1, y2/i]
     const playerBPlays = [/x0, y0/i, /x0, y1/i]
-    let boardMark;
 
-    playerAPlays.forEach((playerAPlay, index) => {
-      boardMark = screen.getByTestId(playerAPlay);
-      userEvent.click(boardMark);
-
-      if (playerBPlays[index] !== undefined) {
-        boardMark = screen.getByTestId(playerBPlays[index]);
-        userEvent.click(boardMark);
-      }
-    });
+    simulatePlayersPlaying(playerAPlays, playerBPlays);
 
     expect(winAlert).toHaveBeenCalledTimes(1);
     expect(winAlert).toHaveBeenCalledWith(expect.stringMatching(`Player A won! Congrats!`));
   });
 
   test('should Player A win when filling the third row', () => {
-    const winAlert = jest.spyOn(window, 'alert');
+    const winAlert = jest.spyOn(window, 'alert').mockImplementation();
     const playerAPlays = [/x2, y0/i, /x2, y1/i, /x2, y2/i]
     const playerBPlays = [/x1, y0/i, /x1, y1/i]
-    let boardMark;
 
-    playerAPlays.forEach((playerAPlay, index) => {
-      boardMark = screen.getByTestId(playerAPlay);
-      userEvent.click(boardMark);
-
-      if (playerBPlays[index] !== undefined) {
-        boardMark = screen.getByTestId(playerBPlays[index]);
-        userEvent.click(boardMark);
-      }
-    });
+    simulatePlayersPlaying(playerAPlays, playerBPlays);
 
     expect(winAlert).toHaveBeenCalledTimes(1);
     expect(winAlert).toHaveBeenCalledWith(expect.stringMatching(`Player A won! Congrats!`));
   });
 })
 
+const simulatePlayersPlaying = (winnerPlays: RegExp[], loserPlays: RegExp[]) => {
+  let boardMark;
+
+  winnerPlays.forEach((winnerPlay, index) => {
+    boardMark = screen.getByTestId(winnerPlay);
+    userEvent.click(boardMark);
+
+    if (loserPlays[index] !== undefined) {
+      boardMark = screen.getByTestId(loserPlays[index]);
+      userEvent.click(boardMark);
+    }
+  });
+}
