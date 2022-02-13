c1cf51f23665e7221c363660fdba244689f87c69
Yemolai <yemolai@gmail.com>
2022-02-13 11:53:07 -0300

    using players as reference, extracting constants

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,7 +1,7 @@
 import {useCallback} from "react";
 import {BoardMark} from "../domain/types/board-mark";
 import {Player} from "../domain/types/player";
-import { defaultMarksText } from "../domain/utils/default-mark-text";
+import { defaultMarksText } from "../domain/constants/default-mark-text";
 
 export type BoardProps = {
   marks: BoardMark[]
--- src/pages/game.page.tsx
+++ src/pages/game.page.tsx
@@ -2,14 +2,16 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Board } from '../components/board';
 import { checkForDraw } from '../domain/conditions/check-for-draw';
 import { checkForWin } from '../domain/conditions/check-for-win';
-import { defaultMarksText } from '../domain/utils/default-mark-text';
+import { defaultMarksText } from '../domain/constants/default-mark-text';
+import { PLAYER_A, PLAYER_B } from '../domain/constants/players';
 import { generateMarks } from '../domain/utils/generate-marks';
 import { sortMarks } from '../domain/utils/sort-marks';
 import { BoardMark } from '../domain/types/board-mark';
+import { Player } from "../domain/types/player";
 
 export const GamePage = (): JSX.Element => {
   const [playing, setPlaying] = useState<boolean>(false)
-  const [playerTurn, setPlayerTurn] = useState<'A' | 'B'>('A')
+  const [playerTurn, setPlayerTurn] = useState<Player>(PLAYER_A)
   const [marks, setMarks] = useState<BoardMark[]>(generateMarks())
 
   const orderedMarks = useMemo(() => marks.slice(0).sort(sortMarks), [marks])
@@ -22,7 +24,7 @@ export const GamePage = (): JSX.Element => {
       {...mark, player: playerTurn},
     ]
     setMarks(newMarks)
-    setPlayerTurn(prevState => prevState === 'A' ? 'B' : 'A')
+    setPlayerTurn(prevState => prevState === PLAYER_A ? PLAYER_B : PLAYER_A)
     setPlaying(false)
   }, [marks, playerTurn, playing])
 
@@ -35,7 +37,7 @@ export const GamePage = (): JSX.Element => {
         alert(`Game over! No one win. It's a DRAW!`)
       }
       setMarks(generateMarks())
-      setPlayerTurn('A')
+      setPlayerTurn(PLAYER_A)
     }
   }, [orderedMarks, playerTurn])
 
