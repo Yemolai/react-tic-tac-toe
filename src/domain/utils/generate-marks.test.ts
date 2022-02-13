f3d8af7867de51930eb819e3f89bc94bc99376c5
Yemolai <yemolai@gmail.com>
2022-02-13 11:57:06 -0300

    adding unit test for generate-marks

---
+++ src/domain/utils/generate-marks.test.ts
@@ -0,0 +1,14 @@
+import {generateMarks} from "./generate-marks";
+
+describe('generate-marks', () => {
+  test('should generate 3x3 grid without players', () => {
+    const marksBoard = generateMarks()
+    for (let y = 0; y < 3; y += 1) {
+      for (let x = 0; x < 3; x += 1) {
+        const mark = marksBoard.find((mark) => mark.x === x && mark.y === y)
+        expect(mark).toBeDefined()
+        expect(mark!.player).not.toBeDefined()
+      }
+    }
+  })
+})
