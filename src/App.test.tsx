3e356aac50f055f16dfa13572202fc2bbadcc8ff
Leonardo Santos Severino <leonardo.osantos@mercadolivre.com>
2022-02-13 00:53:53 -0300

    test: should set the first turn

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('should render the App', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId(/App/i);
+describe("App component", () => {
+  test('should render the App', () => {
+    render(<App />);
+    const linkElement = screen.getByTestId(/App/i);
   
-  expect(linkElement).toBeInTheDocument();
-});
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('should set Player A with the first turn', () => {
+    render(<App />);
+
+    const title = screen.getByText("Player A ❌ turn:")
+    
+    expect(title).toBeInTheDocument();
+  });
+
+})
+
