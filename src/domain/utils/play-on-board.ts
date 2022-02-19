2a883eee192e20f7515d10464e26df0e7f6dea76
Yemolai <yemolai@gmail.com>
2022-02-19 16:28:06 -0300

    better typing error and adding unit test

--- src/domain/utils/play-on-board.ts
+++ src/domain/utils/play-on-board.ts
@@ -6,10 +6,10 @@ export const playOnBoard = (
   board: BoardMark[],
   player: Player,
   position: { x: Position, y: Position },
-): { error?: string, board: BoardMark[] } => {
+): { error?: 'played_tile' | 'invalid_tile', board: BoardMark[] } => {
   const previousMark = board.find(({ x, y }) => position.x === x && position.y === y)
   if (!previousMark) return { board, error: 'invalid_tile' }
-  if (previousMark.player) return { board, error: 'used_tile' }
+  if (previousMark.player) return { board, error: 'played_tile' }
   return {
     board: [
       ...board.filter(mark => mark !== previousMark),
